621251855a88006734158e2c05f3d8129b2e0a70
Aldahir Alonso <aldahir0603@hotmail.com>
2026-07-20 12:15:21 -0600

    Añadiendo rutas nombre, y (codigo usando parseInt y try catch).

--- src/main/java/utez/edu/mx/webappmaster/ParametrosGetServlet.java
+++ src/main/java/utez/edu/mx/webappmaster/ParametrosGetServlet.java
@@ -18,6 +18,7 @@ public class ParametrosGetServlet extends HttpServlet {
         PrintWriter out = resp.getWriter();
 
         String saludo = req.getParameter("saludo");
+        String nombre = req.getParameter("nombre");
         out.println("<!DOCTYPE html>");
         out.println("<html>");
         out.println("<head>");
@@ -28,8 +29,27 @@ public class ParametrosGetServlet extends HttpServlet {
         out.println("</head>");
         out.println("<body>");
         out.println("       <h1>Parametros Get de la url!</h1>");
-        out.println("       <h2>El saludo enviado es: " + saludo + "</h2>");
+        if (saludo != null && nombre != null) {
+
+        out.println("       <h2>El saludo enviado es: " + saludo + " " + nombre + "</h2>");
+        } else if (saludo != null) {
+            out.println("       <h2>El saludo enviado es: " + saludo + "</h2>");
+        } else if (nombre != null) {
+            out.println("       <h2>Hola mi nombre es: " + nombre + "</h2>");
+
+        } else {
+            out.println("<h2>no se han pasado los parametros saludos ni nombre</h2>");
+        }
+        try {
+            int codigo = Integer.parseInt(req.getParameter("codigo"));
+            out.println("<h3>El codigo enviado es : " + codigo + "</h3>");
+
+        } catch (NumberFormatException e) {
+            out.println("<h3>El codigo no se ha enviado es un parametro null</h3>");
+        }
+
         out.println("</body>");
         out.println("</html>");
+        out.close();
     }
 }
